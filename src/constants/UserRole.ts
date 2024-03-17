6c8dcd97863ba0507ef2c8263fe7517ab74bc556
Alberto <alberto.c.pueblas@gmail.com>
2024-03-17 21:34:21 +0100

    add parameters in .env and user role

--- src/constants/UserRole.ts
+++ src/constants/UserRole.ts
@@ -3,5 +3,5 @@ import { Role } from "../models/Role";
 export const UserRoles = {
    ADMIN: { id: 1, name: "admin" } as Role,
    MANAGER: { id: 2, name: "manager" } as Role,
-   CLIENT: { id: 3, name: "user" } as Role,
+   CLIENT: { id: 3, name: "Client" } as Role,
 };
